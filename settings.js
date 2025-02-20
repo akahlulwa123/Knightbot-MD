5315a8174a653aee6e955e74985647da8dc08629
akahlulwa123 <akahlulwasimani11@gmail.com>
2025-02-20 21:09:05 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name: 'Akahlulwa 
-  ownerNumber: '917023951514', //Your number: '0634135504
+  botOwner: 'Knightbot', // Your name: 'Akahlulwa 
+  ownerNumber: '+2763413550', //Your number: '0634135504
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
